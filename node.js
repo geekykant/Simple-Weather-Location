844f79b38f959dd06e3bbd3ef151e4d8ccd7bf50
Sreekant <geekykant@gmail.com>
2018-11-18 00:46:19 +0530

    node 98

--- node.js
+++ node.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var ejs = require('ejs');
 var path = require('path');
+var request = require('request-promise');
+
 var app = express();
 
 //sets the app instance view folder
@@ -11,12 +13,50 @@ app.use(express.static('public'))
 
 //index
 app.get('/',function(req,res){
-	res.render('index',{message:req.param('text')});
+	res.render('index', {location_name:"Hello!", location_data:"Yoo!" });
 });
 
 //about
 app.get('/about',function(req,res){
 	res.render('about');
+});
+
+app.get('/get_weather',function(req,res){
+	var url = "http://api.openweathermap.org/data/2.5/weather";
+	let appid = "af9b69f5927b1ad56e894921325ab85e";
+
+	var lat = req.param('lat');
+	var lon = req.param('lon');
+
+	var request_url = `${url}?appid=${appid}&lat=${lat}&lon=${lon}`;
+
+	console.log("url: " + request_url);
+
+	const options = {  
+	    url: request_url,
+	    method: 'GET',
+	    headers: {'Accept': 'application/json'}
+	};
+
+	// request(options, function(err, res, body) {  
+	//     let json = JSON.parse(body);
+	//     console.log(json['weather'][0]['main'] + "," + json['weather'][0]['description'] + "\n" + json['name'] );
+	// });		
+
+	request(options)
+		.then(function(msg){
+			let json = JSON.parse(msg);
+			console.log(json['weather'][0]['main'] + "," + json['weather'][0]['description'] + "\n" + json['name'] )
+			res.send({location_data: "asldnjasdasda", location_name: "saasdnasd"});
+		})
+		.catch(function(err){
+
+		});
+
+	// res.send({location_data: json['weather'][0]['main'] + "," + json['weather'][0]['description'] , 
+	// 	location_name: json['name']});
+	console.log("DONE LOG!")
+	res.send({location_data: "asd", location_name: "sad"});
 });
 
 app.listen(8081);
---
+++ public/js/custom.js
@@ -0,0 +1,103 @@
+$("#get_location_button").click(function () {
+    $("#get_location_button").addClass('loading');
+    getLocation();
+});
+
+function DoSubmit() {
+    $("#get_location_button").addClass('loading');
+    getLocation();
+    return true;
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        var timeoutVal = 10 * 1000 * 1000;
+        navigator.geolocation.getCurrentPosition(
+            displaySendPosition,
+            displayError, {
+                enableHighAccuracy: true,
+                timeout: timeoutVal,
+                maximumAge: 0
+            }
+        );
+    } else {
+        alert("Geolocation is not supported by this browser");
+    }
+
+    function displaySendPosition(position) {
+        var data = {};
+        data.lat = position.coords.latitude;
+        data.lon = position.coords.longitude;
+
+        $.ajax({
+            type: "GET",
+            url: `get_weather`,
+            data: `lat=${data.lat}&lon=${data.lon}`,
+            success: function (jsonObj) {
+                $("#location_name").text(jsonObj.location_name);
+                $("#location_data").text(jsonObj.location_data);
+
+                $("#get_location_button").removeClass('loading');
+            }
+        });
+
+        //        var xhttp = new XMLHttpRequest();
+        //        xhttp.onreadystatechange = function () {
+        //            if (this.readyState == 4 && this.status == 200) {
+        //                console.log(xhttp.responseText);
+        //                var jsonObj = JSON.parse(xhttp.responseText);
+        //                
+        //                $("#location_name").text(jsonObj.location_name);
+        //                $("#location_data").text(jsonObj.location_data);
+        //
+        //                $("#get_location_button").removeClass('loading');
+        //            }
+        //        };
+        //        xhttp.open("GET", `get_weather?lat=${data.lat}&lon=${data.lon}`, true);
+        //        xhttp.send();
+
+
+        //        $("#lat").val(position.coords.latitude);
+        //        $("#lon").val(position.coords.longitude);
+
+        // add location to database
+        //        db.collection("locations").add({
+        //                location: new firebase.firestore.GeoPoint(position.coords.latitude, position.coords.longitude)
+        //            }).then(function (docRef) {
+        //                console.log("Success!! Location stored in: ", docRef.id);
+        //                receiveFbPosition();
+        //            })
+        //            .catch(function (error) {
+        //                console.error("Error adding document: ", error);
+        //            });
+    }
+
+    // retrive location from database
+    //    function receiveFbPosition() {
+    //        db.collection("locations").get().then(function (querySnapshot) {
+    //            console.log("Listing Firebase Locations:");
+    //            querySnapshot.forEach(function (doc) {
+    //                // doc.data() is never undefined for query doc snapshots
+    //                console.log(doc.id, " => ", doc.data());
+    //            });
+    //        });
+    //    }
+
+    function displayError(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                alert("User denied the request for Geolocation.");
+                break;
+            case error.POSITION_UNAVAILABLE:
+                alert("Location information is unavailable.");
+                break;
+            case error.TIMEOUT:
+                alert("The request to get user location timed out.");
+                break;
+            case error.UNKNOWN_ERROR:
+                alert("An unknown error occurred.");
+                break;
+        }
+        $("#get_location_button").removeClass('loading');
+    }
+}
